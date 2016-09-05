6ccb2daa668b907923a382f87a2111c27bc7738e
EspinolaAbel <espinola.abel.e@gmail.com>
2016-09-05 14:47:58 -0300

    Terminado el punto 1 Arboles binarios de la prácitca 3.

--- Practica_Haskell/TP3/TP3.hs
+++ Practica_Haskell/TP3/TP3.hs
@@ -4,13 +4,16 @@ ejemploConInt :: Tree Int
 ejemploConInt = NodeT 1 (NodeT 2 (Empty) (NodeT 3 (NodeT 4 (Empty) (Empty)) (Empty))) (NodeT 1 (Empty) (Empty))
 
 ejemploConDir :: Tree Dir
-ejemploConDir = NodeT Norte (NodeT Este (Empty) (NodeT Sur (NodeT Oeste (Empty) (Empty)) (Empty))) (NodeT Norte (Empty) (Empty))
+ejemploConDir = NodeT Norte (NodeT Este (NodeT Oeste Empty Empty) (NodeT Sur (NodeT Oeste (Empty) (Empty)) (Empty))) (NodeT Norte (Empty) (Empty))
 
 ejemploConString :: Tree String
 ejemploConString = NodeT "n" (NodeT "ee" (Empty) (NodeT "sss" (NodeT "oooo" (Empty) (Empty)) (Empty))) (NodeT "n" (Empty) (Empty))
 
 ejemploConPersonas :: Tree Persona
 ejemploConPersonas = NodeT p1 (NodeT p2 (Empty) (NodeT p3 (NodeT p4 (Empty) (Empty)) (Empty))) (NodeT p1 (Empty) (Empty))
+
+ejemploConLsDeInt :: Tree [Int]
+ejemploConLsDeInt = NodeT [1,1,1] (NodeT [3,3,3] (NodeT [4,4,4] Empty Empty) (NodeT [5,5,5] (NodeT [6,6,6] Empty Empty) Empty)) (NodeT [2,2,2] Empty Empty)
 
 p1 :: Persona
 p2 :: Persona
@@ -25,36 +28,32 @@ p4 = MKPersona "nombre4" 4
 data Tree a = NodeT a (Tree a) (Tree a) | Empty
 
 --1)
---Proposito: Dado un ́arbol binario de enteros devuelve la suma entre sus elementos.
+--Propósito: Dado unárbol binario de enteros devuelve la suma entre sus elementos.
 sumarT :: Tree Int -> Int
-
 sumarT (Empty) = 0
 sumarT (NodeT n a1 a2) = n + sumarT a1 + sumarT a2
 
 
 --2)
---Proposito: Dado un  ́arbol binario devuelve su cantidad de elementos, es decir, el tama ̃no del  ́arbol (size en ingles).
+--Propósito: Dado un árbol binario devuelve su cantidad de elementos, es decir, el tamaño del árbol (size en inglés).
 sizeT :: Tree a -> Int
-
-sizeT Empty = 0
+sizeT Empty = 1
 sizeT (NodeT x a1 a2) = 1 + sizeT a1 + sizeT a2
 
 
 --3)
---Proposito: Dado un  ́arbol de enteros devuelve un  ́arbol con el doble de cada n ́umero.
+--Propósito: Dado un árbol de enteros devuelve un árbol con el doble de cada número.
 mapDobleT :: Tree Int -> Tree Int
-
 mapDobleT Empty = Empty
 mapDobleT (NodeT n a1 a2) = NodeT (n*2) (mapDobleT a1) (mapDobleT a2)
 
 
 --4)
---Proposito: Dado un  ́arbol de direcciones t devuelve un  ́arbol con la direcci ́on opuesta para cada elemento de t.
+--Propósito: Dado un árbol de direcciones t devuelve un árbol con la dirección opuesta para cada elemento de t.
 --Nota: Utilizar el tipo Dir ya definido.
 data Dir = Norte | Este | Sur | Oeste
 
 opuesto :: Dir -> Dir
-
 opuesto Norte = Sur
 opuesto Este = Oeste
 opuesto Sur = Norte
@@ -68,15 +67,13 @@ instance Eq Dir where
 	_ == _ = False
 
 mapOpuestoT :: Tree Dir -> Tree Dir
-
 mapOpuestoT Empty = Empty
 mapOpuestoT (NodeT d a1 a2) = NodeT (opuesto d) (mapOpuestoT a1) (mapOpuestoT a2) 
 
 
 --5)
---Proposito: Dado un  ́arbol de palabras devuelve un  ́arbol con la longitud de cada palabra.
+--Propósito: Dado un árbol de palabras devuelve un árbol con la longitud de cada palabra.
 mapLongitudT :: Tree String -> Tree Int
-
 mapLongitudT Empty = Empty
 mapLongitudT (NodeT s a1 a2) = NodeT (len s) (mapLongitudT a1) (mapLongitudT a2)  
 
@@ -86,17 +83,15 @@ len (x:xs) = 1 + len xs
 
 
 --6)
---Proposito: Dados un elemento y un arbol binario devuelve True si existe un elemento igual a ese en el ́arbol.
+--Propósito: Dados un elemento y un arbol binario devuelve True si existe un elemento igual a ese en el árbol.
 perteneceT :: Eq a => a -> Tree a -> Bool
-
 perteneceT x Empty = False
 perteneceT x1  (NodeT x a1 a2) = x == x1 || perteneceT x1 a1 || perteneceT x1 a2
 
 
 --7)
---Proposito: Dados un elemento e y un  ́arbol binario devuelve la cantidad de elementos del  ́arbol que son iguales a e.
+--Propósito: Dados un elemento e y un árbol binario devuelve la cantidad de elementos del árbol que son iguales a e.
 aparicionesT :: Eq a => a -> Tree a -> Int
-
 aparicionesT x Empty = 0
 aparicionesT x (NodeT x1 a1 a2) = 	if x == x1 then
 										1 + aparicionesT x a1 + aparicionesT x a2
@@ -105,8 +100,8 @@ aparicionesT x (NodeT x1 a1 a2) = 	if x == x1 then
 
 
 --8)
---Proposito: Dado un  ́arbol de personas devuelve el promedio entre las edades de todas las personas.
---Definir las subtareas que sean necesarias para resolver esta funcion.
+--Propósito: Dado un árbol de personas devuelve el promedio entre las edades de todas las personas.
+--Definir las subtareas que sean necesarias para resolver esta función.
 --Nota: Utilizar el tipo Persona ya definido.
 type Edad = Int
 type Nombre = String
@@ -130,67 +125,117 @@ edad :: Persona -> Edad
 edad (MKPersona _ e) = e
 
 promedioEdadesT :: Tree Persona -> Int
-
 promedioEdadesT a = div (sumarEdadesT a) (sizeT a) 
 
 sumarEdadesT :: Tree Persona -> Int
-
 sumarEdadesT Empty = 0
 sumarEdadesT (NodeT p a1 a2) = edad p + sumarEdadesT a1 + sumarEdadesT a2
 
 
 --9)
---Proposito: Dados dos ́arboles construye un arbol t en el que ambos arboles son hijos de t, y en la raız de t se guarda la suma de todos los elementos de los hijos de t.
+--Propósito: Dados dos árboles construye un arbol t en el que ambos arboles son hijos de t, y en la raız de t se guarda la suma de todos los elementos de los hijos de t.
 engancharYSumaEnRaiz :: Tree Int -> Tree Int -> Tree Int
-
 engancharYSumaEnRaiz a1 a2 = NodeT (sumarT a1 + sumarT a2) a1 a2
 
 
 --10)
---Proposito: Dado un  ́arbol devuelve su cantidad de hojas.
---Nota: una hoja (leaf en ingles) es un nodo que no tiene hijos.
+--Propósito: Dado un árbol devuelve su cantidad de hojas.
+--Nota: una hoja (leaf en inglés) es un nodo que no tiene hijos.
 leaves :: Tree a -> Int
-
 leaves Empty = 0
 leaves (NodeT x Empty Empty) = 1
 leaves (NodeT x a1 a2) = leaves a1 + leaves a2
 
 
---11)
---Proposito: Dado un  ́arbol devuelve su altura.
---Nota: la altura (height en ingl ́es) de un  ́arbol es la cantidad m ́axima de nodos entre la ra ́ız y alguna de sus hojas. La altura de un  ́arbol vac ́ıo es cero y la de una hoja es 1.
+--11)	(i)
+--Propósito: Dado un árbol devuelve su altura.
+--Nota: la altura (height en inglés) de un árbol es la cantidad máxima de nodos entre la raíz y alguna de sus hojas. La altura de un árbol vacío es cero y la de una hoja es 1.
 heightT :: Tree a -> Int
+heightT Empty = -1
+heightT (NodeT _ a1 a2)= 1 + max2 (heightT a1) (heightT a2)
 
-heightT (NodeT _ a1 a2)= if profundidad 0 a1 > profundidad 0 a2 then
-							profundidad 0 a1
-						else
-							profundidad 0 a2
-							
-profundidad :: Int -> Tree a -> Int 
+max2 :: Int -> Int -> Int
+max2 n1 n2 = if n1 > n2 then
+				n1
+			else
+				n2
 
-profundidad n Empty = n
-profundidad n (NodeT _ a1 a2) = if (profundidad (n+1) a1) > (profundidad (n+1) a2) then
-									(profundidad (n+1) a1)
-								else
-									(profundidad (n+1) a2)
-									
---height :: Tree a -> Int
---height Empty = 0
---height (Node x t1 t2) = 1 + (max (height t1) (height t2))
-												
---nodos :: Tree a -> Int 
---nodos Empty = 0
---nodos (NodeT x Empty Empty) = 0
---nodos (NodeT x t1 t2) = 1 + (nodos t1) + (nodos t2)
+--12)
+--Propósito: Dado un árbol devuelve el número de nodos que no son hojas.
+nodes :: Tree a -> Int
+nodes Empty = 0
+nodes (NodeT _ Empty Empty) = 0
+nodes (NodeT _ a1 a2) = 1 + nodes a1 + nodes a2
 --nodos t = (size t) - (leaves t)
 
---espejoT :: Tree a -> Tree a
---espejoT Empty = Empty
---espejoT (NodeT x t1 t2) = NodeT x (espejoT t2) (espejoT t1)
 
---listInOrder :: Tree a -> [a]
---listInOrder Empty = 
---listInOrder (NodeT x t1 t2) = (listInOrder t1) ++ [x] ++ (listInOrder t2)
+--13)
+--Propósito: Dado un árbol devuelve el árbol resultante de intercambiar el hijo izquierdo con el derecho, en cada nodo del árbol.
+espejoT :: Tree a -> Tree a
+espejoT Empty = Empty
+espejoT (NodeT x a1 a2) = NodeT x (espejoT a2) (espejoT a1)
 
 
---HACER 18, 19, y 20 PARA EL LUNES ANTES DE LA CLASE DE E.D.
+--14)
+--Propósito: Dado un árbol devuelve una lista que representa el resultado de recorrerlo en modo in-order.
+--Nota: En el modo in-order primero se procesan los elementos del hijo izquierdo, luego la raiz y luego los elementos del hijo derecho.
+listInOrder :: Tree a -> [a]
+listInOrder Empty = []
+listInOrder (NodeT x a1 a2) = listInOrder a1 ++ [x] ++ listInOrder a2
+
+
+--15)
+--Propósito: Dado un árbol devuelve una lista que representa el resultado de recorrerlo en modo pre-order.
+--Nota: En el modo pre-order primero se procesa la raiz, luego los elementos del hijo izquierdo, a continuación los elementos del hijo derecho.
+listPreOrder :: Tree a -> [a]
+listPreOrder Empty = []
+listPreOrder (NodeT x a1 a2) = [x] ++ listPreOrder a1 ++ listPreOrder a2
+
+
+--16)
+--Propósito: Dado un árbol devuelve una lista que representa el resultado de recorrerlo en modo post-order.
+--Nota: En el modo post-order primero se procesan los elementos del hijo izquierdo, a continuación los elementos del hijo derecho y finalmente la raiz.
+listPosOrder :: Tree a -> [a]
+listPosOrder Empty = []
+listPosOrder (NodeT x a1 a2) = listPosOrder a1 ++ listPosOrder a2 ++ [x]
+
+
+--17)	(i) No es así -----> concatenarListasT a1 ++ [x] ++ concatenarListasT a2
+--Propósito: Dado un árbol de listas devuelve la concatenación de todas esas listas. El recorrido debe ser in-order.
+concatenarListasT :: Tree [a] -> [a]
+concatenarListasT Empty = []
+concatenarListasT (NodeT x a1 a2) = concatenarListasT a1 ++ x ++ concatenarListasT a2
+
+
+
+--18)
+--Propósito: Dados un número n y un árbol devuelve una lista con los nodos de nivel n.
+--Nota: El primer nivel de un árbol (su raı́z) es 0.
+levelN :: Int -> Tree a -> [a]
+levelN 0 (NodeT x a1 a2) = [x]
+levelN n Empty = []
+levelN n (NodeT x a1 a2) = levelN (n-1) a1 ++ levelN (n-1) a2
+
+
+
+--19)
+--Propósito: Dado un árbol devuelve una lista de listas en la que cada elemento representa un nivel de dicho árbol.
+listPerLevel :: Tree a -> [[a]]
+listPerLevel a = recolectarNNiveles (heightT a) a
+
+recolectarNNiveles :: Int -> Tree a -> [[a]]
+recolectarNNiveles 0 a = [levelN 0 a]
+recolectarNNiveles n a = [levelN n a] ++ recolectarNNiveles (n-1) a
+
+
+
+--20)
+--Propósito: Dado un árbol devuelve su ancho (width en inglés), que es la cantidad de nodos del nivel con mayor cantidad de nodos.
+widthT :: Tree a -> Int
+widthT a = maxLen (listPerLevel a)
+
+maxLen :: [[a]] -> Int
+maxLen [] = 0
+maxLen (x:xs) = max2 (len x) (maxLen xs)
+
+
