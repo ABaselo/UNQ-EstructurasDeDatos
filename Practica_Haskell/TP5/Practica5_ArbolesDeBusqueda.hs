a9d84709f0481d41273a3a32533ab2d3d8871a35
EspinolaAbel <espinola.abel.e@gmail.com>
2016-10-14 01:58:14 -0300

    Terminado ejercicio 4-Arboles de búsqueda - elMinimoMayorA

--- Practica_Haskell/TP5/Practica5_ArbolesDeBusqueda.hs
+++ Practica_Haskell/TP5/Practica5_ArbolesDeBusqueda.hs
@@ -4,6 +4,9 @@
 --		2- Los elementos de t2 son mayores que x.
 --		3- t1 y t2 son BST.
 
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+
 --Prop.: Dado un BST inserta un elemento en el árbol.
 --Prec.: El elem dado no pertenece al BST.
 insertBST :: Ord a => a -> Tree a -> Tree a
@@ -13,6 +16,9 @@ insertBST e (NodeT x t1 t2) = 	if e==x
 								else 	if e<x
 										then NodeT x (insertBST e t1) t2
 										else NodeT x t1 (insertBST e t2)
+										
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
 
 --Prop.: Dado un BST dice si el elemento pertenece o no al árbol.
 perteneceBST :: Ord a => a -> Tree a -> Bool
@@ -20,6 +26,9 @@ perteneceBST e EmptyT = False
 perteneceBST e (NodeT x t1 t2) = 	if e <= x
 									then e==x || perteneceBST e t1
 									else perteneceBST e t2
+
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
 
 --Prop.: Dado un BST devuelve un par con el mínimo elemento y el árbol sin el mismo.
 --splitMinBST :: Ord a => Tree a -> (a, Tree a)
@@ -36,8 +45,14 @@ esHoja t1 = False
 elem :: Tree a -> a
 elem (NodeT e _ _) = e
 
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+
 --Prop.: Dado un BST devuelve un par con el máximo elemento y el árbol sin el mismo.
 --splitMaxBST :: Ord a => Tree a -> (a, Tree a)
+
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
 
 --Prop.: Dado un BST y un elemento, devuelve el máximo elemento que sea menor al elemento dado.
 --Prec.: Error en caso de no encontrar el elemento buscado.
@@ -60,23 +75,53 @@ buscarMaximaCotaSuperiorEnT (NodeT e t1 t2) inf sup =
 					else buscarMaximaCotaSuperiorEnT t1 inf sup
 			else buscarMaximaCotaSuperiorEnT t2 inf sup
 
-elementoDelNodoRaiz :: Tree a -> a
-elementoDelNodoRaiz (NodeT e _ _) = e
-
 maxBST :: Tree a -> a
 maxBST (NodeT e EmptyT EmptyT) = e
 maxBST (NodeT e _ t) = maxBST t
 
+-----------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------
+
+--Prop.: Dado un BST y un elemento, devuelve el mínimo elemento que sea mayor al elemento dado.
+--Prec.: Error en caso de no encontrar el elemento buscado.
+elMinimoMayorA :: Ord a => a -> Tree a -> a
+elMinimoMayorA x EmptyT = error "No existe el elemento buscado."
+elMinimoMayorA x (NodeT e t1 t2) = 	if x < e
+									then buscarMinimaCotaInferiorEnT t1 x e
+									else	if e < x
+											then elMinimoMayorA x t2
+											else minBST t2
+
+--Prop.: Dados una cota inferior, una cota superior y un arbol se devuelve el mínimo elemento del arbol que sea mayor a la cota inferior.
+--Prec.: Ninguna.
+buscarMinimaCotaInferiorEnT :: Ord a => Tree a -> a -> a -> a
+buscarMinimaCotaInferiorEnT EmptyT inf sup  = sup
+buscarMinimaCotaInferiorEnT (NodeT e t1 t2) inf sup = 
+			if e < sup
+			then	if inf < e
+					then buscarMinimaCotaInferiorEnT t1 inf e
+					else buscarMinimaCotaInferiorEnT t2 inf sup
+			else buscarMinimaCotaInferiorEnT t1 inf sup
+
 minBST :: Tree a -> a
 minBST (NodeT e EmptyT EmptyT) = e
 minBST (NodeT e t _) = minBST t
-				
---Prop.: Dado un BST y un elemento, devuelve el mínimo elemento que sea mayor al elemento dado.
---elMinimoMayorA :: Ord a => a -> Tree a -> [a]
 
 
 data Tree a = EmptyT | NodeT a (Tree a) (Tree a)
 
 ejBST :: Tree Int
 ejBST = NodeT 9 (NodeT 4 (NodeT 2 (NodeT 1 EmptyT EmptyT) (NodeT 3 EmptyT EmptyT)) (NodeT 5 EmptyT EmptyT)) (NodeT 15 (NodeT 12 EmptyT EmptyT) (NodeT 22 EmptyT EmptyT))
+
+-- ejBST:
+--
+--       __9__
+--      /     \
+--     4      15
+--    / \    /  \
+--   2   5  12  22
+--  / \
+-- 1   3
+
+
 
